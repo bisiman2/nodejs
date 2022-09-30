7a8d87b64142a9cc69ea4fca64b2e5e6e083e093
Bisi Adesanoye <111360618+bisiman2@users.noreply.github.com>
2022-09-30 14:20:15 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.213.28.114:9000/',
+  serverUrl: 'http://34.213.47.69:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
